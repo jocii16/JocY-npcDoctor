dd85cc38526794ad51712296a24ab6b1d365967f
JocY <109790602+JocY16@users.noreply.github.com>
2023-06-01 20:43:30 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,24 @@
-RegisterNetEvent("money", function ()
-    exports.ox_inventory:RemoveItem(source, 'money', Config.amount)
+-- https://github.com/jocY16/ -- jocy5m.tebex.io -- https://discord.gg/3esH84B767
+
+ESX.RegisterServerCallback('jocy-npcDoctor:server:checkmoney', function(source, cb)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if Config.EnableBank == true then
+        local bankamount = xPlayer.getAccount('bank').money
+        if bankamount >= Config.amount then
+            xPlayer.removeAccountMoney('bank', Config.amount)
+            cb(true)
+        else
+            cb(false)
+        end
+    else
+        local cashamount = exports.ox_inventory:Search(source, 'count', 'money')
+        if cashamount >= Config.amount then
+            exports.ox_inventory:RemoveItem(source, 'money', Config.amount)
+            cb(true)
+            
+        else
+            cb(false)
+        end
+    end
 end)
