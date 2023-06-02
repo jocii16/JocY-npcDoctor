3652d90788ffa70bcba3e63af1b50743f4742b3e
JocY <109790602+JocY16@users.noreply.github.com>
2023-06-02 14:10:49 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,9 +1,16 @@
 Config = {}
 
-Config.ped = {
+Config.main = {
+    {
     model = 's_m_m_doctor_01', -- https://docs.fivem.net/docs/game-references/ped-models/
     coords = vec4(168.4956, -988.7020, 29.0867, 353.7385), -- Ped kordi
     zone = vec3(168.4956, -988.7020, 30.0867) -- zone kord = Ped kordi
+    },
+    -- {
+    -- model = 's_m_m_doctor_01', -- https://docs.fivem.net/docs/game-references/ped-models/
+    -- coords = vec4(160.4556, -996.7723, 28.3510, 161.9403), -- Ped kordi
+    -- zone = vec3(160.4556, -996.7723, 29.3510) -- zone kord = Ped kordi
+    -- }
 }
 
 Config.amount = 500 --That's how much he deducts for resuscitation!
