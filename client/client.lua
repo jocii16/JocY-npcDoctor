b8026c63ef766c1090f57c4dd056db4a80270406
JocY <109790602+JocY16@users.noreply.github.com>
2023-06-02 14:11:09 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,49 +1,51 @@
 function pedspawn()
-    lib.RequestModel(Config.ped.model)
-    ped = CreatePed(0, Config.ped.model, Config.ped.coords, false, false)
-    FreezeEntityPosition(ped, true)
-    SetEntityInvincible(ped, true)
-    SetBlockingOfNonTemporaryEvents(ped, true) 
+    for k, v in ipairs(Config.main) do
+        lib.RequestModel(v.model)
+        ped = CreatePed(0, v.model, v.coords, false, false)
+        FreezeEntityPosition(ped, true)
+        SetEntityInvincible(ped, true)
+        SetBlockingOfNonTemporaryEvents(ped, true)
+    end
 end
 
 function zone()
-    local zone = lib.zones.sphere({
-        coords = Config.ped.zone,
-        radius = 2.5,
-        debug = false,
-        inside = function ()
-            if GetEntityHealth(cache.ped) <= 0 then
-               lib.showTextUI("Press [E] to interact") 
-               if IsControlJustPressed(0, 38) then
-                ESX.TriggerServerCallback('jocy-npcDoctor:server:checkmoney', function(have)
-                    if have then
-                    TriggerEvent('esx_ambulancejob:revive')
-                    TriggerServerEvent("jocy-npcDoctor:server:money")
-                    lib.notify({
-                        title = 'Successful interaction',
-                        description = 'You are revived!',
-                        type = 'success',
-                        position = 'top',
-                        duration = 2000
-                    })
-
-                else
-                    lib.notify({
-                        title = 'Failed interaction',
-                        description = 'You dont have enough money!',
-                        type = 'error',
-                        position = 'top',
-                        duration = 2000
-                    })
-
+    for k, v in ipairs(Config.main) do
+        local zone = lib.zones.sphere({
+            coords = v.coords,
+            radius = 2.5,
+            debug = false,
+            inside = function()
+                if GetEntityHealth(cache.ped) <= 0 then
+                    lib.showTextUI("Press [E] to interact")
+                    if IsControlJustPressed(0, 38) then
+                        ESX.TriggerServerCallback('jocy-npcDoctor:server:checkmoney', function(have)
+                            if have then
+                                TriggerEvent('esx_ambulancejob:revive')
+                                TriggerServerEvent("jocy-npcDoctor:server:money")
+                                lib.notify({
+                                    title = 'Successful interaction',
+                                    description = 'You are revived!',
+                                    type = 'success',
+                                    position = 'top',
+                                    duration = 2000
+                                })
+                            else
+                                lib.notify({
+                                    title = 'Failed interaction',
+                                    description = 'You dont have enough money!',
+                                    type = 'error',
+                                    position = 'top',
+                                    duration = 2000
+                                })
+                            end
+                        end)
                     end
-                end)
-               end
-            else
-                lib.hideTextUI()
-            end
-        end,
-    })
+                else
+                    lib.hideTextUI()
+                end
+            end,
+        })
+    end
 end
 
 RegisterNetEvent('esx:playerLoaded')
